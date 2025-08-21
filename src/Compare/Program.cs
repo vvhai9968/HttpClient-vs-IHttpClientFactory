fae0e5d50e608e77ee92a99b96e93adf7d9da2d5
haivv <vuvanhai568@gmail.com>
2025-08-21 22:32:58 +0700

    feat: add HttpClient vs IHttpClientFactory demo

---
+++ src/Compare/Program.cs
@@ -0,0 +1,69 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.MapGet("/start-httpClient", async () =>
+{
+    var count = 0;
+    try
+    {
+        Console.WriteLine("Start");
+        for (var i = 0; i < 100000; i++)
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://localhost:5235/weatherforecast");
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+            count++;
+        }
+
+        Console.WriteLine("Done");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        return Results.BadRequest($"{e.Message} Max: {count}");
+    }
+
+    return Results.Ok();
+});
+
+app.MapGet("/start-IHttpClientFactory", async (IHttpClientFactory httpClientFactory) =>
+{
+    try
+    {
+        Console.WriteLine("Start");
+        for (var i = 0; i < 100; i++)
+        {
+            Task.Delay(3000).Wait(); var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync("http://localhost:5235/weatherforecast");
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+            Console.WriteLine(Environment.NewLine);
+        }
+
+        Console.WriteLine("Done");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+    return Results.Ok();
+});
+
+app.Run();
